1850b3c314cb13bd084c611d0a52f8054b9cdee1
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-18 21:48:58 +0300

    Добавляет стили для хедеров экранов

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,14 +3,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MainScreen } from '../screens/MainScreen';
 import { PostScreen } from '../screens/PostScreen';
+import { THEME } from '../theme';
+import { Platform } from 'react-native';
 
 const Stack = createStackNavigator();
+const defaultScreenOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? THEME.MAIN_COLOR : '#fff',
+    },
+    headerTintColor: Platform.OS === 'android' ? '#fff' : THEME.MAIN_COLOR,
+};
 
 export const AppNavigation = () => {
 
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Main">
+            <Stack.Navigator 
+                initialRouteName="Main"
+                screenOptions={defaultScreenOptions}
+            >
                 <Stack.Screen 
                     name="Main" 
                     component={MainScreen}
