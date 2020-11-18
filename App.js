74ab87b797ab16129c0467cb667474e222d19feb
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-18 20:59:49 +0300

    Подключает шрифты при загрузке приложения

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import { AppLoading } from 'expo';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { bootstrap } from './src/bootstrap';
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [isReady, setIsReady] = useState(false);
+
+    if (!isReady) {
+        return (
+            <AppLoading 
+                onFinish={() => setIsReady(true)}
+                startAsync={bootstrap}
+            />
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
