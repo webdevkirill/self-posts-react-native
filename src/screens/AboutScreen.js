8c230ceb959409d7e83412c674eb378c7345d07a
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-19 01:41:01 +0300

    Добавляет контент на экран о приложении

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -5,7 +5,9 @@ export const AboutScreen = () => {
 
     return (
         <View style={styles.wrapper}>
-            <Text>AboutScreen</Text>
+            <Text style={styles.text}>Лучшее приложение для личных заметок!</Text>
+            <Text style={styles.text}>Версия приложения <Text style={styles.version}>1.0.0</Text></Text>
+            <Text style={styles.text}>Автор <Text style={styles.version}>Кирилл Фёдоров</Text></Text>
         </View>
     )
 }
@@ -15,5 +17,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    text: {
+        fontFamily: 'open-regular',
+        marginBottom: 5
+    },
+    version: {
+        fontFamily: 'open-bold'
     }
 })
