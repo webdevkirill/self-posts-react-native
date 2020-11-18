0f54bfcdb4fb94d1fac75b8edabfe25abf4e9960
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-18 22:37:45 +0300

    Выводит и стилизует данные на экране поста

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView, Button, Alert } from 'react-native';
+import { THEME } from '../theme';
 
 export const PostScreen = ({navigation, route}) => {
-    const {id, booked, date, img, text} = route;
-    const postId = navigation.id;
+    const {id, booked, date, img, text} = route.params;
+    
+    const removeHandler = () => {
+        Alert.alert(
+            'Удаление поста',
+            'Вы точно хотите удалить пост?',
+            [
+                {
+                    text: 'Отменить',
+                    style: 'cancel'
+                },
+                {
+                    text: 'Удалить',
+                    onPress: () => {},
+                    style: 'destructive'
+                }
+            ],
+            {cancelable: false}
+        )
+    }
 
     return (
-        <View style={styles.wrapper}>
-            <Text>PostScreen</Text>
-        </View>
+        <ScrollView>
+            <Image source={{uri: img}} style={styles.image} />
+            <View style={styles.textWrap}>
+                <Text style={styles.title}>{text}</Text>
+            </View>
+            <Button title="Удалить" color={THEME.DANGER_COLOR} onPress={removeHandler} />
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    textWrap: {
+        padding: 10,
+    },
+    title: {
+        fontFamily: 'open-regular'
     }
 })
