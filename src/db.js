65b8e97237b6d563d1f9209109736ba1fab27dba
webdevkirill <kirill-fjodorov00000@yandex.ru>
2020-11-21 19:14:18 +0300

    Создает статический метод для загрузки постов из базы данных и использует его

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import * as SQLite from 'expo-sqlite';
 
-const db = SQLite.openDatabase('post.db');
+const db = SQLite.openDatabase('posts.db');
 
 export class DB {
     static init() {
@@ -13,6 +13,19 @@ export class DB {
                 )
             })
             
+        })
+    }
+
+    static getPosts() {
+        return new Promise((resolve, reject) => {
+            db.transaction(tx => {
+                tx.executeSql(
+                    'SELECT * FROM posts',
+                    [],
+                    (_, result) => resolve(result.rows._array),
+                    (_, error) => reject(error)
+                )
+            })
         })
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
 import { postReducer } from './reducers/postReducer';
 
 const rootReducer = combineReducers({postReducer});
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
